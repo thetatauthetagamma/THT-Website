d784e7ba712ebbebd0d97caaa4e5433a28838ca4
owilloug <owilloug@umich.edu>
2023-11-12 15:55:52 -0500

    fix 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,7 +7,7 @@ export default function Custom404() {
       className="h-screen bg-cover bg-center"
       style={{ backgroundImage: `url(${homepage})` }}
     >
-      <h1 className='text-black text-lg sm:text-xl md:text-4xl lg:text-6xl xl:text-8xl sm:text-lg font-bold pt-20 pb-20 pl-20 pr-20 text-center'>
+      <h1 className='text-black text-lg sm:text-xl md:text-2xl lg:text-4xl xl:text-6xl sm:text-lg font-bold pt-20 pb-20 pl-20 pr-20 text-center'>
         AHHHHHHHHHHHHHHHH!!!! ..... This page doesn't exist :)
       </h1>
       <Image className='px-10' src={homepage} alt="homepage" />
