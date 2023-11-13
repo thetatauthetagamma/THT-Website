e4481d1964614895244e11379cb81f0d46ac769e
HariChidam <hchidam@umich.edu>
2023-11-13 10:34:50 -0500

    favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
 import { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [supabase] = useState(() => createPagesBrowserClient())
@@ -11,6 +12,10 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
       <Layout>
+        <Head>
+          <link rel="icon" href="/tht-logo.png" type="image/png" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
         <Component {...pageProps} />
       </Layout>
     </SessionContextProvider>
