63d436bf8fa88144ba3cb9323a2d81cbc7ea838a
HariChidam <hchidam@umich.edu>
2023-11-13 11:41:23 -0500

    update typeing speed

--- components/Typewriter.tsx
+++ components/Typewriter.tsx
@@ -6,14 +6,25 @@ interface TypewriterProps {
 }
 
 const Typewriter: React.FC<TypewriterProps> = ({ words }) => {
-    return (
-      <TypeAnimation
-        sequence={words}
-        wrapper="span"
-        speed={20}
-        repeat={Infinity}
-      />
+  const sequenceWithDelays = words.flatMap((word, index) => {
+    // Add a delay after each word except for the last one
+    return index === words.length - 1 ? [word] : [word, 750];
+  });
+
+  return (
+    <TypeAnimation
+      sequence={sequenceWithDelays}
+      wrapper="span"
+      speed={1}
+      repeat={Infinity}
+    />
   );
 };
 
 export default Typewriter;
+
+
+
+
+
+
