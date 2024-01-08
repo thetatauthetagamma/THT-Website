8ce5a89ce0c9cb0e6add73b19f935033a9ad5cc1
katemcg <katemcg@umich.edu>
2024-01-08 00:15:55 -0800

    Creating new pledge on front end works

--- components/newPledge.js
+++ components/newPledge.js
@@ -19,34 +19,33 @@ const NewPledgeTile = () => {
   const [committeeSO, setCommitteeSO] = useState(0)
   const [completed, setCompleted] = useState(0)
 
-  const [firstname, setFirstname] = useState('First Name')
-  const [lastname, setLastname] = useState('Last Name')
-  const [major, setMajor] = useState('Major')
-  const [pronouns, setPronouns] = useState('Pronouns')
-  const [year, setYear] = useState('Grad Year')
+  const [firstname, setFirstname] = useState('')
+  const [lastname, setLastname] = useState('')
+  const [major, setMajor] = useState('')
+  const [pronouns, setPronouns] = useState('')
+  const [year, setYear] = useState('')
+  const [uniqname, setUniqname] = useState('')
 
   const [userID, setUserID] = useState('')
 
-  const [socialHours, setSocialHours] = useState(0);
-  const [academicHours, setAcademicHours] = useState(0);
+  const [socialHours, setSocialHours] = useState(0)
+  const [academicHours, setAcademicHours] = useState(0)
 
-
-  const [isAdmin, setIsAdmin] = useState(false);
-  const [isEditing, setIsEditing] = useState(false);
-  const [editMode, setEditMode] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false)
+  const [isEditing, setIsEditing] = useState(false)
+  const [editMode, setEditMode] = useState(false)
   const [editableFields, setEditableFields] = useState({
     uniqname: false,
     firstname: false,
     lastname: false,
     year: false,
-    pronouns:false,
+    pronouns: false,
     major: false,
-    imageUrl: false,
-  });
-
+    imageUrl: false
+  })
 
   //key = supabase column, value = display value
- 
+
   useEffect(() => {
     const fetchSession = async () => {
       try {
@@ -60,61 +59,57 @@ const NewPledgeTile = () => {
     fetchSession()
   }, [])
 
- 
-
   const handleCreatePledge = async () => {
     try {
       // Send a request to create a new pledge with the provided uniqname
-      const { data, error } = await supabase
-        .from('Pledges')
-        .insert([{ uniqname: newPledgeUniqname, firstname: newPledgeFirst, lastname:newPledgeLast,
-        pronouns: newPledgePronouns }]);
+      const { data, error } = await supabase.from('Pledges').insert([
+        {
+          uniqname: uniqname,
+          firstname: firstname,
+          lastname: lastname,
+          pronouns: pronouns,
+          year: year,
+          major: major,
+        }
+      ])
 
       // If successful, update the state to include the new pledge
-      if (data) setPledges([...pledges, { uniqname: newPledgeUniqname, firstname: '' }]);
+      if (data)
+        setPledges([...pledges, { uniqname: newPledgeUniqname, firstname: '' }])
 
       // Reset the newPledgeUniqname state
-      setNewPledgeUniqname('');
-      setNewPledgeFirst('');
-      setNewPledgeLast('');
-      setNewPledgePronouns('');
+      setUniqname('')
+      setFirstname('')
+      setLastname('')
+      setPronouns('')
+      setMajor('')
+      setYear('')
     } catch (error) {
-      console.error(error);
+      console.error(error)
       // Handle errors if needed
     }
-  };
+  }
 
   useEffect(() => {
     const fetchAdminRole = async () => {
       try {
         const { data, error } = await supabase
-        .from('Brothers')
-        .select('adminrole')
-        .eq('email', userID)
+          .from('Brothers')
+          .select('adminrole')
+          .eq('email', userID)
 
-      if (error) {
-        throw error
-      }
-      if (data) {  
-        if(data[0].adminrole == 'parent'){
-          setIsAdmin(true);
+        if (error) {
+          throw error
         }
-    }
-  } catch (error) {}
+        if (data) {
+          if (data[0].adminrole == 'parent') {
+            setIsAdmin(true)
+          }
+        }
+      } catch (error) {}
     }
     fetchAdminRole()
   }, [userID])
-
-
-  
-
-
-
-
-
- 
-
-
 
   const handleSave = async () => {
     try {
@@ -124,213 +119,241 @@ const NewPledgeTile = () => {
         .update([
           {
             academicHours,
-            socialHours,
-          },
+            socialHours
+          }
         ])
-        .eq('uniqname', pledge);
-  
+        .eq('uniqname', pledge)
+
       if (!error) {
-        console.log('Pledge hours updated successfully');
+        console.log('Pledge hours updated successfully')
         // Optionally reset editableFields state to hide input fields
         setEditableFields({
           academicHours: false,
           socialHours: false
-        });
-  
-
-      } 
-     
-      else {
-        console.error('Error updating profile:', error.message);
+        })
+      } else {
+        console.error('Error updating profile:', error.message)
       }
     } catch (error) {
-      console.error('Error updating profile:', error.message);
+      console.error('Error updating profile:', error.message)
     }
-  };
-
-
+  }
 
   const handleFieldEdit = () => {
-    
-    setEditableFields((prevFields) => ({
+    setEditableFields(prevFields => ({
       ...prevFields,
       firstname: !prevFields.firstname,
       lastname: !prevFields.lastname,
+      uniqname: !prevFields.uniqname,
       year: !prevFields.year,
       major: !prevFields.major,
-      phone: !prevFields.phone,
-      linkedin: !prevFields.linkedin,
+      pronouns: !prevFields.major,
       imageUrl: !prevFields.imageUrl,
       currentClasses: !prevFields.currentClasses
-    }));
+    }))
     if (!editableFields.academicHours) {
-      setAcademicHours(academicHours); // Use the initial state or fetch it from the server
+      setAcademicHours(academicHours) // Use the initial state or fetch it from the server
     }
-  
+
     if (!editableFields.socialHours) {
-      setSocialHours(socialHours); // Use the initial state or fetch it from the server
+      setSocialHours(socialHours) // Use the initial state or fetch it from the server
     }
-    setEditMode(!editMode);
-  };
+    setEditMode(!editMode)
+  }
 
-
-
-
-  
   return (
     <div className='bg-gray-100 p-2 rounded-2xl mb-4'>
-     {editMode? ( 
-     <div className = 'w-full flex flex-col md:flex-row items-center'>
-      <div className='flex flex-col items-center md:w-3/12'>
-        <div className='mb-2 w-40 h-40'>
-          {imageUrl ? (
-            <img
-              src={imageUrl}
-              alt='Pledge'
-              className='rounded-full w-full h-full object-cover'
-            />
-          ) : (
-            <Image
-              src={thtlogo}
-              alt='logo'
-              className='rounded-full w-full h-full object-cover'
-            />
-          )}
-        </div>
-        <div className='text-center w-full'>
-          <div className = "flex md:flex-row flex-col items-center w-full px-2 mr-4">
-          <div className='text-md  text-center md:mr-2 w-1/2'>
-                {editableFields.firstname? (
+      {editMode ? (
+        <div className='w-full flex flex-col md:flex-row items-center'>
+          <div className='flex flex-col items-center md:w-3/12'>
+            <div className='mb-2 w-40 h-40'>
+              {imageUrl ? (
+                <img
+                  src={imageUrl}
+                  alt='Pledge'
+                  className='rounded-full w-full h-full object-cover'
+                />
+              ) : (
+                <Image
+                  src={thtlogo}
+                  alt='logo'
+                  className='rounded-full w-full h-full object-cover'
+                />
+              )}
+            </div>
+            <div className='text-center w-full'>
+              <div className='text-md  text-center py-1 '>
+                {editableFields.uniqname ? (
                   <input
                     type='text'
-                    placeholder={firstname}
-                    value={firstname}
-                    onChange={(e) => setFirstname(e.target.value)}
-                    className='whitespace-nowrap  text-center border-2 border-[#8b000070]'
+                    placeholder={'uniqname'}
+                    value={uniqname}
+                    onChange={e => setUniqname(e.target.value)}
+                    className='whitespace-nowrap  text-center border-2 border-[#8b000070] w-full'
                   />
                 ) : (
-                  `${firstname}`
+                  `${uniqname}`
                 )}
               </div>
-              <div className='text-md text-center md:mr-2 w-1/2'>
-                {editableFields.lastname? (
-                  <input
-                    type='text'
-                    placeholder={lastname}
-                    value={lastname}
-                    onChange={(e) => setLastname(e.target.value)}
-                    className='whitespace-nowrap  text-center border-2 border-[#8b000070]'
-                  />
-                ) : (
-                  `${firstname}`
-                )}
-              </div>
-          </div>
-          
-          <p>
-            {year} | {pronouns} | {major}
-          </p>
-          <div className='flex flex-col md:flex-row items-center justify-evenly w-full'>
-                
-              
-                  <div className='flex flex-row m-2 items-center'>
-                    <button className='font-bold mr-2 text-md bg-[#8b000070] p-2 rounded-md text-center' onClick={handleFieldEdit}>
-                      Cancel
-                    </button>
-                    <button className='font-bold text-md bg-[#8b000070] p-2 rounded-md text-center' onClick={handleSave}>
-                      Create Pledge 
-                    </button>
-                  </div>
-              
+              <div className='flex md:flex-row flex-col items-center w-full '>
+                <div className='text-md  text-center  md:w-1/2 md:pr-1 py-1'>
+                  {editableFields.firstname ? (
+                    <input
+                      type='text'
+                      placeholder={'first name'}
+                      value={firstname}
+                      onChange={e => setFirstname(e.target.value)}
+                      className='whitespace-nowrap  text-center border-2 border-[#8b000070] w-full'
+                    />
+                  ) : (
+                    `${firstname}`
+                  )}
+                </div>
+                <div className='text-md text-center md:w-1/2 md:pl-1 py-1'>
+                  {editableFields.lastname ? (
+                    <input
+                      type='text'
+                      placeholder={'last name'}
+                      value={lastname}
+                      onChange={e => setLastname(e.target.value)}
+                      className='whitespace-nowrap  text-center border-2 border-[#8b000070] w-full'
+                    />
+                  ) : (
+                    `${lastname}`
+                  )}
+                </div>
               </div>
 
-        </div>
-      </div>
-      <div className='flex flex-col items-center w-9/12'>
-      <div className='flex flex-col md:flex-row items-center justify-evenly w-full pb-2'>
-        <div className='flex flex-col items-center p-2 '>
-          <p className='text-sm font-bold mb-1'># of Interviews</p>
-          <p className='text-sm'>{ 0}</p>
-        </div>
-        <div className='flex flex-col items-center md:border-x-2 border-black p-2'>
-          <p className='text-sm text-center font-bold mb-1'># of PD Activities</p>
-          <p className='text-sm'>{0}</p>
-        </div>
-        <div className='flex flex-col items-center p-2'>
-          <p className='text-sm text-center font-bold mb-1'># of Committee Signoffs</p>
-          <p className='text-sm'>{0}</p>
-        </div>
-        <div className='flex flex-col items-center md:border-x-2 border-black p-2'>
-          <p className='text-sm text-center font-bold mb-1'># of Social Hours</p>
-          <p className='text-sm'>
-         0
-          </p>
-        </div>
-        <div className='flex flex-col items-center p-2'>
-          <p className='text-sm text-center font-bold mb-1'># of Academic Hours</p>
-          <p className='text-sm'>      <p className='text-sm'>
-         0
-          </p></p>
-        </div>
-      </div>
-        <div className='flex flex-col items-center w-full p-2'>
-          <ProgressBar
-            className='w-full'
-            completed={0}
-            bgColor='#22c55e'
-            height='40px'
-          />
-        </div>
-        <div className='flex flex-col md:flex-row items-center m-4 w-full justify-evenly'>
-          {/* Interview Button (to the left) */}
-        
-          <div className='flex items-center justify-center  m-2 md:w-1/3'>
-           
-           
+              <div className='flex lg:flex-row flex-col items-center w-full'>
+                <div className='text-md  text-center  lg:w-1/3 lg:pr-1 py-1'>
+                  {editableFields.year && (
+                    <input
+                      type='text'
+                      placeholder={'Grad Year'}
+                      value={year}
+                      onChange={e => setYear(e.target.value)}
+                      className='whitespace-nowrap  text-center border-2 border-[#8b000070] w-full'
+                    />
+                  )}
+                </div>
+                <div className='text-md text-center lg:w-1/3 lg:p-1 py-1'>
+                  {editableFields.pronouns ? (
+                    <input
+                      type='text'
+                      placeholder={'pronouns'}
+                      value={pronouns}
+                      onChange={e => setPronouns(e.target.value)}
+                      className='whitespace-nowrap  text-center border-2 border-[#8b000070] w-full'
+                    />
+                  ) : (
+                    `${pronouns}`
+                  )}
+                </div>
+                <div className='text-md text-center lg:w-1/3 lg:pl-1 py-1'>
+                  {editableFields.major ? (
+                    <input
+                      type='text'
+                      placeholder={'major'}
+                      value={major}
+                      onChange={e => setMajor(e.target.value)}
+                      className='whitespace-nowrap  text-center border-2 border-[#8b000070] w-full'
+                    />
+                  ) : (
+                    `${major}`
+                  )}
+                </div>
+              </div>
+              <div className='flex flex-col md:flex-row items-center justify-evenly w-full'>
+                <div className='flex flex-row m-2 items-center'>
+                  <button
+                    className='font-bold mr-2 text-md bg-[#8b000070] p-2 rounded-md text-center'
+                    onClick={handleFieldEdit}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className='font-bold text-md bg-[#8b000070] p-2 rounded-md text-center'
+                    onClick={handleCreatePledge}
+                  >
+                    Create Pledge
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
+          <div className='flex flex-col items-center w-9/12'>
+            <div className='flex flex-col md:flex-row items-center justify-evenly w-full pb-2'>
+              <div className='flex flex-col items-center p-2 '>
+                <p className='text-sm font-bold mb-1'># of Interviews</p>
+                <p className='text-sm'>{0}</p>
+              </div>
+              <div className='flex flex-col items-center md:border-x-2 border-black p-2'>
+                <p className='text-sm text-center font-bold mb-1'>
+                  # of PD Activities
+                </p>
+                <p className='text-sm'>{0}</p>
+              </div>
+              <div className='flex flex-col items-center p-2'>
+                <p className='text-sm text-center font-bold mb-1'>
+                  # of Committee Signoffs
+                </p>
+                <p className='text-sm'>{0}</p>
+              </div>
+              <div className='flex flex-col items-center md:border-x-2 border-black p-2'>
+                <p className='text-sm text-center font-bold mb-1'>
+                  # of Social Hours
+                </p>
+                <p className='text-sm'>0</p>
+              </div>
+              <div className='flex flex-col items-center p-2'>
+                <p className='text-sm text-center font-bold mb-1'>
+                  # of Academic Hours
+                </p>
+                <p className='text-sm'>
+                  {' '}
+                  <p className='text-sm'>0</p>
+                </p>
+              </div>
+            </div>
+            <div className='flex flex-col items-center w-full p-2'>
+              <ProgressBar
+                className='w-full'
+                completed={0}
+                bgColor='#22c55e'
+                height='40px'
+              />
+            </div>
+            <div className='flex flex-col md:flex-row items-center m-4 w-full justify-evenly'>
+              {/* Interview Button (to the left) */}
 
-          <div className='flex items-center justify-center m-2 md:w-1/3'>
-            {/* Committee Dropdown */}
-            
+              <div className='flex items-center justify-center  m-2 md:w-1/3'></div>
 
-            {/* Submit Button */}
-           
+              <div className='flex items-center justify-center m-2 md:w-1/3'>
+                {/* Committee Dropdown */}
+
+                {/* Submit Button */}
+              </div>
+            </div>
           </div>
-
-
         </div>
-      </div>
-      </div>) : (
-
-    
-<div
-onClick={() => handleFieldEdit()}
-className="cursor-pointer"
->
-<div className='mb-2 w-40 h-40'>
-
-            <Image
-              src={plus}
-              alt='logo'
-              className='rounded-full w-full h-full object-cover'
-            />
-      
+      ) : (
+        <div className='flex flex-col items-center md:w-3/12'>
+          <div onClick={() => handleFieldEdit()} className='cursor-pointer'>
+            <div className='mb-2 w-40 h-40'>
+              <Image
+                src={plus}
+                alt='logo'
+                className='rounded-full w-full h-full object-cover'
+              />
+            </div>
+          </div>
         </div>
-</div>)}
-
-    
+      )}
     </div>
   )
 }
 
 export default NewPledgeTile
-
-
-
-
-
-
-
 
 /*          {isAdmin && (
             <div>
