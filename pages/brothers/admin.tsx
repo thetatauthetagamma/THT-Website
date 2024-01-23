83f8c3670a4ae61965f30c3cedd2be489eb8c30e
katemcg <katemcg@umich.edu>
2024-01-23 11:54:11 -0500

    listing options for dif admin to implement

--- pages/brothers/admin.tsx
+++ pages/brothers/admin.tsx
@@ -1,5 +1,5 @@
 import BroNavBar from "@/components/BroNavBar";
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import supabase from '@/supabase';
 
 export default function BroResources() {
@@ -17,42 +17,52 @@ export default function BroResources() {
 
     fetchSession()
   }, [])
-  
 
-useEffect(() => {
-  const fetchAdminRole = async () => {
-    try {
-      const { data, error } = await supabase
-        .from('Brothers')
-        .select('adminrole')
-        .eq('email', userID)
 
-      if (error) {
-        throw error
-      }
-      if (data) {
+  useEffect(() => {
+    const fetchAdminRole = async () => {
+      try {
+        const { data, error } = await supabase
+          .from('Brothers')
+          .select('adminrole')
+          .eq('email', userID)
 
-        setAdminRole(data[0].adminrole);
-  
-      }
-    } catch (error) {}
-  }
+        if (error) {
+          throw error
+        }
+        if (data) {
 
-  fetchAdminRole();
-}, [userID])
-  
+          setAdminRole(data[0].adminrole);
+
+        }
+      } catch (error) { }
+    }
+
+    fetchAdminRole();
+  }, [userID])
+
   return (
     <div className="flex md:flex-row flex-col flex-grow border-b-2 border-[#a3000020]">
-    
-    <BroNavBar isPledge={false}/>
-    <div className ="flex flex-col m-5">
-    <h1 className="flex flex-center text-lg">As {adminRole}, you can do the following things:</h1>
-    
-    <h1> Set PD Sign Offs</h1>
-    <h1> Set Committee Sign Offs</h1>
-    <h1> Set Required Number of Academic Hours</h1>
-    <h1> Set Required Number of Social Hours</h1>
+
+      <BroNavBar isPledge={false} />
+      <div className="flex flex-col m-5">
+        <h1 className="flex flex-center text-lg">As {adminRole}, you can do the following things:</h1>
+        {adminRole == 'parent' && (
+          <div>
+            <h1> Set PD Sign Offs</h1>
+            <h1> Set Committee Sign Offs</h1>
+            <h1> Set Required Number of Academic Hours</h1>
+            <h1> Set Required Number of Social Hours</h1>
+          </div>
+        )}
+        {(adminRole == 'regent' || adminRole == 'scribe') && (
+
+          <div>
+            <h1> Initiate Pledges</h1>
+            <h1> Update EBoard</h1>
+          </div>
+        )}
+      </div>
     </div>
-</div>
   )
 }
