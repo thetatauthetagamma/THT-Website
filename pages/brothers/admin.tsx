dd334d406bfb5ebc4f7f5590abe875bc723bf162
katemcg <katemcg@umich.edu>
2024-01-31 19:36:11 -0500

    formatted

--- pages/brothers/admin.tsx
+++ pages/brothers/admin.tsx
@@ -14,7 +14,13 @@ export default function BroResources() {
   const [eboardEditingMode, setEboardEditingMode] = useState<boolean>(false);
   const [eboardMembers, setEboardMembers] = useState<BrotherData[]>([]);
   const [newRoleNames, setNewRoleNames] = useState<{ [key: string]: string }>({});
-
+  const eboardPositions = [
+    { role: "regent", label: "Regent" },
+    { role: "vice", label: "Vice Regent" },
+    { role: "scribe", label: "Scribe" },
+    { role: "treasurer", label: "Treasurer" },
+    { role: "corsec", label: "Corsec" },
+  ];
   interface PledgeData {
     uniqname: string;
     firstname: string;
@@ -68,6 +74,36 @@ export default function BroResources() {
       console.error('Error fetching EBoard members:', error);
     }
   };
+  const handleArchiveClasses = async () => {
+    try {
+      // Fetch all brothers
+      const { data: allBrothers, error } = await supabase
+        .from('Brothers')
+        .select('userid, classes, archivedclasses');
+
+      if (error) {
+        throw error;
+      }
+
+      // Filter brothers who have classes in the classes column
+      const brothersWithClasses = allBrothers.filter(brother => brother.classes && brother.classes.length > 0);
+
+      // Update the archivedclasses column for each brother
+      for (const brother of brothersWithClasses) {
+        const updatedClasses = [...(brother.archivedclasses || []), ...(brother.classes || [])];
+
+        // Update the archivedclasses column in the database
+        await supabase
+          .from('Brothers')
+          .update({ archivedclasses: updatedClasses })
+          .eq('userid', brother.userid);
+      }
+
+      console.log('Classes archived successfully');
+    } catch (error) {
+      console.error('Error archiving classes:', error);
+    }
+  };
 
   const handleEboardSubmit = async (adminRole: string) => {
     try {
@@ -80,7 +116,7 @@ export default function BroResources() {
         .eq('userid', newRoleNames[adminRole]);
 
       // Task 2: Delete the adminRole from the current brother holding that role
-      
+
       for (const member of eboardMembers) {
         if (member.adminrole === adminRole && (member.userid != newRoleNames[adminRole])) {
           await supabase
@@ -270,16 +306,16 @@ export default function BroResources() {
         )}
         {(adminRole === 'regent' || adminRole === 'scribe') && (
           <div className="flex flex-col">
-            
+
             <h1 className="flex flex-center text-4xl">Thank you for your service brother {adminRole} 🫡</h1>
             <h1 className="flex flex-center text-lg my-2">As {adminRole}, you can do the following things:</h1>
             {rollEditingMode ? (
-              <div className="bg-[#fff0f0] p-4 rounded-md flex flex-col m-2">
+              <div className="bg-[#fff0f0] p-4 rounded-md flex flex-col m-2 items-center">
                 <h1 className='text-lg font-bold'>Assign pledges roll numbers:</h1>
                 {pledges.map(pledge => (
-                  <div key={pledge.uniqname} className="flex flex-col sm:flex-row m-4 w-1/2">
-                    <p className="m-2 sm:w-1/2">{`${pledge.firstname} ${pledge.lastname}`}</p>
-                    <div className="flex justify-end w-full">
+                  <div key={pledge.uniqname} className="flex flex-col sm:flex-row m-4 w-full justify-center">
+                    <p className="m-2 whitespace-nowrap text-end">{`${pledge.firstname} ${pledge.lastname}`}</p>
+                    <div className="flex self-center">
                       <input
                         type="text"
                         placeholder="Assign Roll Number"
@@ -290,73 +326,50 @@ export default function BroResources() {
                   </div>
                 ))}
                 <div className='flex flex-row'>
-                <button onClick={handleCancel} className="font-bold mr-2 text-md bg-[#8b000070] p-2 rounded-md text-center w-24 m-2">Cancel</button>
-                <button onClick={handleSubmit} className="font-bold mr-2 text-md bg-[#8b000070] p-2 rounded-md text-center w-24 m-2">Submit</button>
+                  <button onClick={handleCancel} className="font-bold mr-2 text-md bg-[#8b000070] p-2 rounded-md text-center w-24 m-2">Cancel</button>
+                  <button onClick={handleSubmit} className="font-bold mr-2 text-md bg-[#8b000070] p-2 rounded-md text-center w-24 m-2">Submit</button>
                 </div>
               </div>
-              
+
             ) : (
               <button onClick={handleInitiatePledges} className="font-bold mr-2 text-md bg-[#8b000070] p-2 rounded-md text-center my-2 w-48">Initiate Pledges</button>
             )}
-       
+
             {eboardEditingMode ? (
               <div className='bg-[#fff0f0] p-4 rounded-md flex flex-col m-2'>
-                <div className=' flex flex-col md:flex-row items-start justify-start'> 
-                <h1 className='text-lg font-bold'>Update Eboard and Committee Heads (Update yourself last!):</h1>
-                <button onClick={handleCancelEBoard} className="font-bold mr-2 text-md bg-red-400 p-1 rounded-md text-center w-36 mx-2">Done Updating</button>
+                <div className='flex flex-col md:flex-row flex-center justify-start mx-auto'> {/* Added mx-auto for centering */}
+                  <h1 className='text-lg font-bold text-center'>Update Eboard and Committee Heads (Update yourself last!):</h1>
                 </div>
-                <p>Current regent: {currentRegent ? `${currentRegent.firstname} ${currentRegent.lastname}` : 'No regent found'}</p>
-                <div className="flex w-64">
-                  <input
-                    type="text"
-                    placeholder="Input new regents uniqname"
-                    className="whitespace-nowrap text-center border-2 border-[#8b000070] w-full my-2"
-                    onChange={(e) => handleRoleNameChange("regent", e.target.value)}
-                  />
+                <div className='flex flex-col justify-center items-center'>
+                  {eboardPositions.map(({ role, label }) => (
+                    <div key={role} className='flex flex-col items-center'>
+                      <p>
+                        Current {label}: {eboardMembers.find(member => member.adminrole === role) ?
+                          `${eboardMembers.find(member => member.adminrole === role)?.firstname} ${eboardMembers.find(member => member.adminrole === role)?.lastname}` :
+                          'No one found'
+                        }
+                      </p>
+                      <div className='flex flex-row'>
+                        <div className="flex w-64">
+                          <input
+                            type="text"
+                            placeholder={`Input new ${role} uniqname`}
+                            className="whitespace-nowrap text-center border-2 border-[#8b000070] w-full my-2"
+                            onChange={(e) => handleRoleNameChange(role, e.target.value)}
+                          />
+                        </div>
+                        <button onClick={() => handleEboardSubmit(role)} className="font-bold mr-2 text-md bg-[#8b000070] self-center rounded-md text-center w-24 mx-1">Submit</button>
+                      </div>
+                    </div>
+                  ))}
+                  <div className="text-center mx-auto"> {/* Added mx-auto for centering */}
+                    <button onClick={handleCancelEBoard} className="font-bold mr-2 text-md bg-red-400 p-1 rounded-md text-center w-36 mt-2">Done Updating</button>
+                  </div>
                 </div>
-                <button onClick={() => handleEboardSubmit("regent")} className="font-bold mr-2 text-md bg-[#8b000070] p-2 rounded-md text-center w-24">Submit</button>
-                <p>Current vice regent: {currentVice?.firstname} {currentVice?.lastname}</p>
-                <div className="flex w-64">
-                  <input
-                    type="text"
-                    placeholder="Input new vice uniqname"
-                    className="whitespace-nowrap text-center border-2 border-[#8b000070] w-full my-2"
-                    onChange={(e) => handleRoleNameChange("vice", e.target.value)}
-                  />
-                </div>
-                <button onClick={() => handleEboardSubmit("vice")} className="font-bold mr-2 text-md bg-[#8b000070] p-2 rounded-md text-center w-24">Submit</button>
-                <p>Current scribe: {currentScribe?.firstname} {currentScribe?.lastname}</p>
-                <div className="flex w-64">
-                  <input
-                    type="text"
-                    placeholder="Input new scribe uniqname"
-                    className="whitespace-nowrap text-center border-2 border-[#8b000070] w-full my-2"
-                    onChange={(e) => handleRoleNameChange("scribe", e.target.value)}
-                  />
-                </div>
-                <button onClick={() => handleEboardSubmit("scribe")} className="font-bold mr-2 text-md bg-[#8b000070] p-2 rounded-md text-center w-24">Submit</button>
-                <p>Current treasurer: {currentTreasurer?.firstname} {currentTreasurer?.lastname}</p>
-                <div className="flex w-64">
-                  <input
-                    type="text"
-                    placeholder="Input new treasurer uniqname"
-                    className="whitespace-nowrap text-center border-2 border-[#8b000070] w-full my-2"
-                    onChange={(e) => handleRoleNameChange("treasurer", e.target.value)}
-                  />
-                </div>
-                <button onClick={() => handleEboardSubmit("treasurer")} className="font-bold mr-2 text-md bg-[#8b000070] p-2 rounded-md text-center w-24">Submit</button>
-                <p>Current Corsec: {currentCorSec?.firstname} {currentCorSec?.lastname}</p>
-                <div className="flex w-64">
-                  <input
-                    type="text"
-                    placeholder="Input new corsec uniqname"
-                    className="whitespace-nowrap text-center border-2 border-[#8b000070] w-full my-2"
-                    onChange={(e) => handleRoleNameChange("corsec", e.target.value)}
-                  />
-                </div>
-                <button onClick={() => handleEboardSubmit("corsec")} className="font-bold mr-2 text-md bg-[#8b000070] p-2 rounded-md text-center w-24">Submit</button>
-                
-                
+
+
+
+
 
               </div>
             ) : (
@@ -370,7 +383,7 @@ export default function BroResources() {
           <div className='flex flex-col'>
             <h1 className="flex flex-center text-4xl">Hi Kohl</h1>
             <h1 className="flex flex-center text-lg my-2">As Academic Head, you can do the following things:</h1>
-            <button onClick={handleInitiatePledges} className="font-bold mr-2 text-md bg-[#8b000070] p-2 rounded-md text-center">Archive Classes</button>
+            <button onClick={handleArchiveClasses} className="font-bold mr-2 text-md bg-[#8b000070] p-2 rounded-md text-center">Archive Classes</button>
           </div>
         )}
         {(adminRole == 'treasurer') && (
